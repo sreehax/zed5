ff81f9a8b43886bc03b01d0ba01e6e682e43a46d
Sreehari Sreedev <sreeharisreedev1@gmail.com>
2021-06-15 16:28:38 -0700

    build assembly example in tree

--- build.zig
+++ build.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const baseline_rv64 = std.Target.riscv.cpu.baseline_rv64;
+const FeatureSet = std.Target.Cpu.Feature.Set;
 
 pub fn build(b: *std.build.Builder) void {
     // Standard target options allows the person running `zig build` to choose
@@ -11,7 +13,8 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("zemu", "src/main.zig");
+    // the main emulator executable
+    const exe = b.addExecutable("zed5", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.addPackage(.{
@@ -19,6 +22,20 @@ pub fn build(b: *std.build.Builder) void {
         .path = .{ .path = "libs/zig-clap/clap.zig" }
     });
     exe.install();
+
+    // the add/addi asm example
+    const add = b.addExecutable("add.elf", null);
+    var f = FeatureSet.empty;
+    const comp: std.Target.riscv.Feature = .c;
+    f.addFeature(@enumToInt(comp));
+    add.setTarget(.{
+        .cpu_arch = .riscv64,
+        .os_tag = .freestanding,
+        .cpu_model = .{ .explicit = &baseline_rv64 },
+        .cpu_features_sub = f
+    });
+    add.addAssemblyFile("src/add.S");
+    add.install();
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
